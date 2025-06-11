4d6b7a3dd2feb1cdafd6371386da43657787e250
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-12 00:32:20 +0700

    pylatihan Operator.py

---
+++ Operator.py
@@ -0,0 +1,14 @@
+angka1 = 3
+angka2 = 5
+angka3 = angka1 + angka2
+angka4 = angka3 - 2
+angka5 = 25
+
+print("Hasil penjumlahan: " + str(angka1) + str(angka2))
+print(angka1%angka2)
+print(2**3)
+print(angka3)
+print(angka4)
+
+import math
+print(math.sqrt(angka5))
