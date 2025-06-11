7721d3f330e0aed39347df7b4d7a46916198ebad
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-11 19:50:49 +0700

    add learning variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+nama = "Ed"
+angka = 5
+angkaKoma = 3.14
+jurusan = input("Jurusan: ")
+
+print("Nama saya adalah " + nama)
+print("Jurusan yang saya ambil adalah " + jurusan)
+print("Angka favorit saya adalah " + str(angka))
+print(angka + angkaKoma)
