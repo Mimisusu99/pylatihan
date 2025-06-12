ad400ee03aca48327bd6c859672d259b6f945654
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-12 15:09:05 +0700

    conditional statements

---
+++ conditionalStatement.py
@@ -0,0 +1,23 @@
+x = 5
+y = 10
+z = 15
+
+print(not (x > y))
+print(x < y)
+print(x < y and y < z)
+
+if x == y:
+    print("x sama dengan y")
+
+if x < y:
+    print("x lebih kecil dari y")
+
+a = 20
+b = 40
+
+if (a > b):
+    print("a lebih besar dari b")
+elif (a == b):
+    print("a sama dengan b")
+else:
+    print("a lebih kecil dari b")
