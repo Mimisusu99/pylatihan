0f72e96059bb7b875c0cb8d515c466c353e51a93
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-12 15:38:38 +0700

    looping

---
+++ looping.py
@@ -0,0 +1,26 @@
+# for j in range(6):
+#     print("looping!")
+
+# start = 0 , stop = 12 , step = 2
+# for i in range(0, 12, 2):
+#     print(i)
+
+# for d in range(10, 20, 3):
+#     if d == 19:
+#         break
+        
+#     print(d)
+
+# for e in range(10, 20, 3):
+#     if e == 16:
+#         continue
+        
+#     print(e)
+
+uang = 1000000
+
+while uang > 0:
+    print("masih punya uang " + str(uang))
+    uang -= 100000
+print("uang sudah habis!")
+    
