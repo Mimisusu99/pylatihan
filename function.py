90e8e92887b8a4624c07743b1add17b3880d29ce
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-12 23:22:24 +0700

    Add function

---
+++ function.py
@@ -0,0 +1,24 @@
+# function name: nama dari function
+# argument: nilai yang diberikan ke function
+# function body: isi dari sebuah function
+# return value: hasil yang dikembalikan oleh function
+
+# create and call
+
+# a function named greet that takes one argument
+def greet(name):
+    print("Hello, " + name + "!")
+    print("Welcome to the world of functions.")
+    print("Have a great day!")
+
+greet("Alice")
+
+# Passing Arguments
+
+#Lambda Function
+
+def add5(x):
+    return x+5
+
+print(add5(10))  
+# Lambda function to add 5 to a number
